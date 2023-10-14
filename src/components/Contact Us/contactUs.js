0ac742de971e877e3ca9d3932843ebfc71d41961
shubhankarpatil <shubhankar.a.patil@gmail.com>
2023-10-14 18:53:35 +0530

    contact us

--- src/components/Contact Us/contactUs.js
+++ src/components/Contact Us/contactUs.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import CallRoundedIcon from '@mui/icons-material/CallRounded';
+import { Snackbar, SnackbarContent } from "@mui/material";
 import "./contactUs.css"
 
 const ContactUs = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [message, setMessage] = useState("");
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -22,6 +25,21 @@ const ContactUs = () => {
     }
   };
 
+  const handleCloseSnackBar = () => {
+    setSnackBarOpen(false);
+  }
+
+  const handleCopy = (text) => {
+    navigator.clipboard.writeText(text)
+    .then(() => {
+      setMessage('Data has been copied');
+      setSnackBarOpen(true)
+    })
+    .catch((error) => {
+      console.error('Unable to copy data to clipboard', error)
+    })
+  }
+
   return (
     <div
       onMouseEnter={handleMouseEnter}
@@ -38,14 +56,19 @@ const ContactUs = () => {
       <span>Contact Us</span>
       {isHovered && (
         <div className="tooltip">
-          <div onClick={() => handleContactClick('mobile')} style={{ cursor: 'pointer', color: "black", fontSize: "1rem", display: 'flex', alignItems: 'center', paddingBottom: ".4rem" }}>
-            <CallRoundedIcon fontSize='small' style={{ marginRight: '.5rem' }} /> 123456789
+          <div style={{ color: "black", fontSize: "1rem", display: 'flex', alignItems: 'center', paddingBottom: ".4rem" }}>
+            <CallRoundedIcon onClick={() => handleContactClick('mobile')} fontSize='small' style={{ marginRight: '.5rem' }} /> 
+            <p onClick={() => handleCopy('123456789')} style={{cursor: 'pointer'}}>123456789</p>
           </div>
-          <div onClick={() => handleContactClick('mobile')} style={{ cursor: 'pointer', color: "black", fontSize: "1rem", display: 'flex', alignItems: 'center' }}>
-          <EmailRoundedIcon fontSize='small' style={{ marginRight: '.5rem' }} /> manthrafoods@gmail.com
+          <div style={{ color: "black", fontSize: "1rem", display: 'flex', alignItems: 'center', marginTop: '-1rem' }}>
+            <EmailRoundedIcon onClick={() => handleContactClick('email')}  fontSize='small' style={{ marginRight: '.5rem' }} /> 
+            <p onClick={() => handleCopy('manthrafoods@gmail.com')} style={{cursor: 'pointer'}}>manthrafoods@gmail.com</p>
           </div>
         </div>
       )}
+      <Snackbar open={snackBarOpen} autoHideDuration={2000} onClose={handleCloseSnackBar} anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
+        <SnackbarContent sx={{ backgroundColor: "#2ecc71" }} message={message} />
+      </Snackbar>
     </div>
   );
 };
