865ca68f8c1fabd455128d403bba2a182e2e8d43
shubhankarpatil <shubhankar.a.patil@gmail.com>
2023-10-14 12:51:43 +0530

    Updated path for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,20 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import ContactUs from '../Contact Us/contactUs'
 
-const Header = (props) => {
+const Header = () => {
   return (
     <header className="header">
       <div className="brand">
-        <Link to="/" style={{ textDecoration: "none", color: "inherit", fontSize: "2.5rem" }}>Manthra</Link>
+        <Link to="/" style={{ textDecoration: "none", color: "black", fontSize: "2.5rem" }}>Manthra</Link>
       </div>
 
       <nav className="navigation">
         {/* <Link to="/about">About Us</Link> */}
-        {/* <Link to="/contact">Contact Us</Link> */}
-        <Link>Recepies</Link> 
-        <Link to="/cart" className="cart">
-          Cart
-        </Link>
+        {/* <Link>Recepies</Link>  */}
+        <Link to="/cart"> Cart </Link>
         <Link to="/franchise-enquiry">Franchise Enquiry</Link>
+        <ContactUs />
       </nav>
     </header>
   );
